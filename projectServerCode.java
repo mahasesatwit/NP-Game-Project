071d4c65217dd7caef64ab4a7db8138881c3fbad
bogonissATWIT <bogoniss@wit.edu>
2021-11-22 19:03:02 -0500

    Added multithreading!

--- projectServerCode.java
+++ projectServerCode.java
@@ -1,3 +1,4 @@
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class projectServerCode {
@@ -16,6 +17,27 @@ public class projectServerCode {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ServerSocket serverSocket;
+		try {
+			serverSocket = new ServerSocket(1234);
+			System.out.println("This server is ready to receive");
+
+			while (true) {
+				Socket connectionSocket = serverSocket.accept();
+				ClientRequest clientRequest = new ClientRequest(connectionSocket);
+				Thread thread = new Thread(clientRequest);
+				thread.start();
+			}
+		}
+			catch (Exception ex) {
+			// TODO Auto-generated catch block
+			System.out.println("Error!\r\n");
+		}
+
+	
+		
+
 
 	}
 
