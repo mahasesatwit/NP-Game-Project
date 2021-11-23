f1e8fd22c97200597aff82dbccb15eec18a2dfb8
bogonissATWIT <bogoniss@wit.edu>
2021-11-23 12:27:54 -0500

    lots of changes...
    I reorganized the project files themselves and sectioned each method off
    to make it easier to make changes to individual methods. I also added
    short descriptions to each method explaining what it does. I also added
    some methods and classes from our chat room lab that will help us with
    the chat room and lobby aspect of the game.

--- projectServerCode.java
+++ projectServerCode.java
@@ -1,32 +1,79 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class projectServerCode {
-	static class ClientRequest implements Runnable {
-		private String name;
-		private Scanner in;
-		private PrintWriter out;
-		private Socket socket;
-		
-		ClientRequest(Socket c){
-			socket = c;
-			
-		}
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//								TCP-Tac-Toe ---- Server Code									  //
+//								Sam Bogonis & Shemar Mahase										  //
+//							Network Programming - Final Project									  //
+//									projectServerCode											  //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class projectServerCode {
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// People class: defines individual players and ties their information to their thread connection
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	static class Players {
+		String name;
+		Socket connection;
+		
+		Players(Socket c){
+			connection = c;
+		}
+		
+		public void setName(String n) {
+			name = n;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public Socket getConnection() {
+			return connection;
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////////	
+// Lists: keeps track of players, previous messages, etc.
+////////////////////////////////////////////////////////////////////////////////////////////////////	
+	static List<Players> players = new ArrayList<>();
+	static List<String> prevMsg = new ArrayList<>();
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// ClientRequest: server-side socket handling
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	static class ClientRequest implements Runnable {
+			Socket connectionSocket;
+			
+			ClientRequest(Socket c){
+				connectionSocket = c;
+				
+		}
+			
+//////////////////////////////////////////////////////////////////////////////////////////////////// 
+// run(): handles users inputs, where the magic happens
+////////////////////////////////////////////////////////////////////////////////////////////////////
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		
+		
 		try {
-			in = new Scanner(socket.getInputStream());
-			out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream out = new DataOutputStream(connectionSocket.getOutputStream());
 			
 			while (true) {
 				
-				
+				//put something here
 				
 				
 				
@@ -44,8 +91,9 @@ public class projectServerCode {
 			
 			
 	}
-	
-	
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// main(String[] args): sets up multithreading and adds new users to players 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -59,6 +107,8 @@ public class projectServerCode {
 				ClientRequest clientRequest = new ClientRequest(connectionSocket);
 				Thread thread = new Thread(clientRequest);
 				thread.start();
+				players.add(new Players(connectionSocket));
+				
 			}
 		}
 			catch (Exception ex) {
@@ -71,8 +121,52 @@ public class projectServerCode {
 
 
 	}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// sendToAll(String msg, String name): sends a name and string message to users connected to the server 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static void sendToAll(String msg, String name) throws IOException {
+		if(prevMsg.size() < 5) {
+			prevMsg.add(name+": "+msg);
+		}
+		else {
+			prevMsg.remove(0);
+			prevMsg.add(name+": "+msg);
+		}
+		for(int i = 0; i < players.size();i++) {
+			if(name.equals(players.get(i).getName())) {
+				continue;
+			}
+			DataOutputStream outToClient = new DataOutputStream(players.get(i).getConnection().getOutputStream());
+			outToClient.writeBytes(name+": "+msg);
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// sentToAll(String msg): sends a string message to users connected to the server
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static void sendToAll(String msg) throws IOException {
+		if(prevMsg.size() < 5) {
+			prevMsg.add(msg);
+		}
+		else {
+			prevMsg.remove(0);
+			prevMsg.add(msg);
+		}
+		for(int i = 0; i < players.size();i++) {
+			DataOutputStream outToClient = new DataOutputStream(players.get(i).getConnection().getOutputStream());
+			outToClient.writeBytes(msg);
+		}
+	}
 
-	
-
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// closeConnection: closes TCP connection of specific user, removes from players
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static void closeConnection(String name) {
+		for(int i = 0; i < players.size();i++) {
+			if(name.equals(players.get(i).getName())) {
+				players.remove(i);
+			}
+		}
+	}
+////////////////////////////////////////////////////////////////////////////////////////////////////
  }
 }
